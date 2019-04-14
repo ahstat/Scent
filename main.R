c60c7586cc9b7c5a43cbae88c55db6126e913251
Alexis Huet <ahstat@qq.com>
2019-04-14 21:05:06 +0200

    first 2d plot

--- main.R
+++ main.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 library(mvtnorm)
 library(abind)
+library(tidyr)
+library(ggplot2)
 source("helpers/1_density.R")
 source("helpers/2_dynamic.R")
 source("helpers/3_plot.R")
@@ -32,13 +34,23 @@ idx_particle = 1
 plot(arrayout[1,idx_particle,], arrayout[2,idx_particle,])
 lines(arrayout[1,2,], arrayout[2,2,])
 
-df = as.data.frame.table(arrayout, base = list(paste0("dim", 1:dim(arrayout)[1]),
-                                          paste0("part", 1:dim(arrayout)[2]),
-                                          as.character(1:dim(arrayout)[3])))
-df[,3] = as.numeric(df[,3])
+convert_array_to_df = function(arrayout) {
+  df = as.data.frame.table(arrayout, base = list(paste0("dim", 1:dim(arrayout)[1]),
+                                                 paste0("part", 1:dim(arrayout)[2]),
+                                                 as.character(1:dim(arrayout)[3])))
+  df[,3] = as.numeric(df[,3])
+  
+  df1 <- spread(data = df, key = Var1, value = Freq)
+  head(df1)
+  names(df1) = c("particle", "iteration", "dim1", "dim2")
+  
+  return(df1)
+}
 
-plot()
+df = convert_array_to_df(arrayout)
+
 # En vue de dessus, trajectoire de chaque particule (tous les x pas)
+ggplot(df, aes(x = dim1, y = dim2, colour = particle)) + geom_path()
 
 ############################
 # One-dimensional plotting #
